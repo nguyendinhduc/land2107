01525c982af35e36a739de9c4d6597c30ea32799
ducnd <ducnd@vmodev.com>
2022-03-07 17:03:19 +0700

    finish function A

--- src/main/java/com/t3h/buoi12/TestingGitOne.java
+++ src/main/java/com/t3h/buoi12/TestingGitOne.java
@@ -3,4 +3,10 @@ package com.t3h.buoi12;
 public class TestingGitOne {
     private int valueA;
 
+
+    private int valueB;
+    private int valueC;
+    private int valueD;
+    private int valueE;
+
 }
